cf4d537213194104c7fe9454a03c6a5e25cbd275
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-03-26 05:11:24 +0900

    Update setCluster.sh

--- setCluster.sh
+++ setCluster.sh
@@ -1,4 +1,4 @@
-mkdir ~/m1; mkdir ~/m2; mkdir ~/m3; mkdir ~/m4; mkdir ~/m5; mkdir ~/m6
+mkdir ~/m1; mkdir ~/m2; mkdir ~/m3; mkdir ~/m4; mkdir ~/m5; mkdir ~/m6; mkdir ~/m7; mkdir ~/m8
 
 cd ~/m1; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m1; vagrant reload
 cd ~/m2; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m2; vagrant reload
@@ -6,3 +6,5 @@ cd ~/m3; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/V
 cd ~/m4; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m4; vagrant reload
 cd ~/m5; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m5; vagrant reload
 cd ~/m6; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m6; vagrant reload
+cd ~/m7; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m7; vagrant reload
+cd ~/m8; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m8; vagrant reload
