797beb81b169ddaf1b0f7361fdcc33446a226afe
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-03-26 05:17:47 +0900

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt-get install -y git
+sudo apt-get install -y tmux
+rm -rf ~/.tmux.conf
+cp ~/Vag/.tmux.conf ~/
+bash ~/Vag/alias
+sudo apt install virtualbox
+curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
+sudo apt install ./vagrant_2.2.9_x86_64.deb
