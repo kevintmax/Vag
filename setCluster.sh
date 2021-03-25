f6830a644155cd74f32613864208ceac86782653
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-03-26 04:51:31 +0900

    Create setCluster.sh

---
+++ setCluster.sh
@@ -0,0 +1,8 @@
+mkdir ~/m1; mkdir ~/m2; mkdir ~/m3; mkdir ~/m4; mkdir ~/m5; mkdir ~/m6
+
+cd ~/m1; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m1; vagrant reload
+cd ~/m2; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m2; vagrant reload
+cd ~/m3; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m3; vagrant reload
+cd ~/m4; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m4; vagrant reload
+cd ~/m5; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m5; vagrant reload
+cd ~/m6; vagrant init generic/ubuntu2004; vagrant up; rm Vagrantfile; cp ~/Vag/Vagrantfile ~/m6; vagrant reload
