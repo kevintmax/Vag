dca6b87a31bb8d946f45edecde41be16d7a7ee2c
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-03-26 05:53:12 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+cd
 sudo apt update
 sudo apt-get install -y tmux
 rm -rf ~/.tmux.conf
 cp ~/Vag/.tmux.conf ~/
 bash ~/Vag/alias
 sudo apt install virtualbox -y
+
 curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
 sudo apt install ./vagrant_2.2.9_x86_64.deb
